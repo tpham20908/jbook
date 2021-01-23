eb94603284381b0804e9aa15547ac9ee332152b1
tungpham <tpham20908@gmail.com>
2021-01-23 10:55:32 -0500

    axios request to tiny-test-pkg

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,31 +1,42 @@
 import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
 
 export const unpkgPathPlugin = () => {
 	return {
 		name: 'unpkg-path-plugin',
 		setup(build: esbuild.PluginBuild) {
 			build.onResolve({ filter: /.*/ }, async (args: any) => {
-				console.log('onResole', args);
-				return { path: args.path, namespace: 'a' };
+				console.log('onResolve', args);
+				const { path } = args;
+				if (path === 'index.js') {
+					return { path: args.path, namespace: 'a' };
+				} else if (path === 'tiny-test-pkg') {
+					return {
+						path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+						namespace: 'a',
+					};
+				}
 			});
 
 			build.onLoad({ filter: /.*/ }, async (args: any) => {
 				console.log('onLoad', args);
+				const { path } = args;
 
-				if (args.path === 'index.js') {
+				if (path === 'index.js') {
 					return {
 						loader: 'jsx',
 						contents: `
-              import message from './message';
+              const message = require('tiny-test-pkg');
               console.log(message);
             `,
 					};
-				} else {
-					return {
-						loader: 'jsx',
-						contents: 'export default "hi there!"',
-					};
 				}
+
+				const { data } = await axios.get(path);
+				return {
+					loader: 'jsx',
+					contents: data,
+				};
 			});
 		},
 	};
